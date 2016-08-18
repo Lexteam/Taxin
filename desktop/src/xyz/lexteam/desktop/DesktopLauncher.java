3c69e4f3076fef32353c3bb2d8630dc6e3dc120c
charlesstewart <tomdr1001@outlook.com>
2016-08-18 16:41:49 +0100

    Initial commit

---
+++ desktop/src/xyz/lexteam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package xyz.lexteam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import xyz.lexteam.TaxinGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TaxinGame(), config);
+	}
+}
