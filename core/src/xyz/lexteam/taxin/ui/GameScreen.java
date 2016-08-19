10b5b34b74e5bdd0a46b8d4b0be6551e6f21eb2e
charlesstewart <tomdr1001@outlook.com>
2016-08-19 15:33:04 +0100

    Begin ui.

---
+++ core/src/xyz/lexteam/taxin/ui/GameScreen.java
@@ -0,0 +1,57 @@
+package xyz.lexteam.taxin.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class GameScreen implements Screen {
+    Stage stage;
+
+    public GameScreen() {
+        stage = new Stage();
+
+        stage.addActor(new Image(new Texture("badlogic.jpg")));
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.draw();
+        stage.act();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
